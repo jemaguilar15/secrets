667a6d172f6763f71ccc935229a2ebc3faa4cb5e
John <design.johnaguilar@gmail.com>
2020-08-06 17:00:54 +0800

    Add process.env.PORT

--- app.js
+++ app.js
@@ -238,7 +238,7 @@ app.get("/logout", (req, res) => {
     res.redirect("/");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server are running at port 3000...");
       
 });
